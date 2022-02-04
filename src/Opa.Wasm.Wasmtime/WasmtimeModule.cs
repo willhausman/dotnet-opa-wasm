a404e2f69ec2f36d48a61ce551513c4a31973cb7
Will Hausman <will.jhausman@outlook.com>
2022-02-04 23:04:10 +0000

    runtime things

---
+++ src/Opa.Wasm.Wasmtime/WasmtimeModule.cs
@@ -0,0 +1,48 @@
+using Opa.Wasm.Abstractions;
+using Wasmtime;
+
+namespace Opa.Wasm.Wasmtime;
+
+public class WasmtimeModule : IWasmModule
+{
+	private bool disposedValue;
+    private readonly Engine engine;
+    private readonly Module module;
+
+    private WasmtimeModule(Engine engine, Module  module)
+    {
+        this.engine = engine;
+        this.module = module;
+    }
+
+	public IWasmInstance CreateInstance()
+	{
+		throw new NotImplementedException();
+	}
+
+    public static WasmtimeModule FromFile(string filePath)
+    {
+        var engine = new Engine();
+        return new WasmtimeModule(engine, Module.FromFile(engine, filePath));
+    }
+
+	protected virtual void Dispose(bool disposing)
+	{
+		if (!disposedValue)
+		{
+			if (disposing)
+			{
+                engine.Dispose();
+                module.Dispose();
+			}
+
+			disposedValue = true;
+		}
+	}
+
+	public void Dispose()
+	{
+		Dispose(disposing: true);
+		GC.SuppressFinalize(this);
+	}
+}
